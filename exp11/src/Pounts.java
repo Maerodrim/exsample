3683914f8db2a1f749531c0807192af44f32c288
Maerodrim <stas.serg.99@mail.ru>
2019-09-09 21:08:09 +0400

    MAKE 1.8

--- exp11/src/Pounts.java
+++ exp11/src/Pounts.java
@@ -15,4 +15,7 @@ public class Pounts {
         return o;}
     public static Point inverse(Point a){Point o= new Point((1/a.pointX),(1/a.pointY),(1/a.pointZ));
         return o;}
+    public static double scalarProduct(Point a, Point b){ return ((a.pointX*b.pointX)+(a.pointY*b.pointY)+(a.pointZ*b.pointZ)); }
+    public static Point vectorProduct(Point a, Point b){
+        return new Point(a.pointY*b.pointZ - a.pointZ*b.pointY, a.pointZ*b.pointX - a.pointX*b.pointZ, a.pointX*b.pointY-a.pointY*b.pointX);}
 }
