821045a90f948c0ad4aa179005c55c6f5e089158
Maerodrim <stas.serg.99@mail.ru>
2019-09-09 20:57:59 +0400

    MAKE 1.6 and 1.7

--- exp11/src/Pounts.java
+++ exp11/src/Pounts.java
@@ -11,4 +11,8 @@ public class Pounts {
         return o;}
     public static Point enlarge(Point a, double b){Point o= new Point((a.pointX*b),(a.pointY*b),(a.pointZ*b));
         return o;}
+    public static Point opposite(Point a){Point o= new Point((-a.pointX),(-a.pointY),(-a.pointZ));
+        return o;}
+    public static Point inverse(Point a){Point o= new Point((1/a.pointX),(1/a.pointY),(1/a.pointZ));
+        return o;}
 }
