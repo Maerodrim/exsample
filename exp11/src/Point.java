2a1f18bff2ea7d51a4c6c1155ec5cf2a83a698f4
Maerodrim <stas.serg.99@mail.ru>
2019-09-09 20:13:50 +0400

    MAKE 1.4

--- exp11/src/Point.java
+++ exp11/src/Point.java
@@ -1,7 +1,7 @@
 public class Point { 
-    final double pointX;
-    final double pointY;
-    final double pointZ;
+    public final double pointX;
+    public final double pointY;
+    public final double pointZ;
     public Point (double pointX , double pointY , double pointZ){
         this.pointX=pointX;
         this.pointY=pointY;
---
+++ exp11/src/Pounts.java
@@ -0,0 +1,12 @@
+public class Pounts {
+    private Pounts() {;}
+    public static Point sum(Point a, Point b){
+        Point o= new Point((a.pointX+b.pointX),(a.pointY+b.pointY),(a.pointZ+b.pointZ));
+        return o;}
+    public static Point subtract(Point a, Point b){ Point o= new Point((a.pointX-b.pointX),(a.pointY-b.pointY),(a.pointZ-b.pointZ));
+        return o;}
+    public static Point multiply(Point a, Point b){ Point o= new Point((a.pointX*b.pointX),(a.pointY*b.pointY),(a.pointZ*b.pointZ));
+        return o;}
+    public static Point divide(Point a, Point b){ Point o= new Point((a.pointX/b.pointX),(a.pointY/b.pointY),(a.pointZ/b.pointZ));
+        return o;}
+}
