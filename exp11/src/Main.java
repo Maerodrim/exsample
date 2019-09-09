3d5b02b8fd307d95cbefdc64f4b088b955d7d848
Maerodrim <stas.serg.99@mail.ru>
2019-09-09 23:50:50 +0400

    1.13 фишка в использование локальной переменной в функции, которая не влияет на main

--- exp11/src/Main.java
+++ exp11/src/Main.java
@@ -1,7 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Person person = new Person();
+        person.setFirstName("Arkadiy");
+        int number = 5;
+        checkInt(number);
+        checkPerson(person);
+        System.out.println(number);
+        System.out.println(person.getFirstName());
     }
+
+    private static void checkInt(int number) {
+        number = 10;
+        System.out.println(number);
+    }
+
+    private static void checkPerson(Person person) {
+        person.setFirstName("Oleg");
+        System.out.println(person.getFirstName());
+    }
+
 }
 
