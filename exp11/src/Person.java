b66fd4c835e2d41baf81fe258bd0ef7022630895
Maerodrim <stas.serg.99@mail.ru>
2019-09-09 21:37:06 +0400

    clean cod

--- exp11/src/Person.java
+++ exp11/src/Person.java
@@ -1,10 +1,4 @@
-public class exp2 {
-
-    public static void main(String[] args) {
-        // write your code here
-    }
-}
-class Person2 {
+class Person {
     private int passportId;
     private String firstName;
     private String lastName;
@@ -23,16 +17,16 @@ class Person2 {
         this.passportId=passportId;
         return passportId;
     };
-    public Person2(int passportId){
+    public Person(int passportId){
         this.passportId=passportId;
     };
-    public Person2(){
+    public Person(){
     }
-    public Person2(String firstName , String lastName){
+    public Person(String firstName , String lastName){
         this.firstName=firstName;
         this.lastName=lastName;
     }
-    public Person2(String firstName , String lastName , int passportId){
+    public Person(String firstName , String lastName , int passportId){
         this.firstName=firstName;
         this.lastName=lastName;
         this.passportId=passportId;
