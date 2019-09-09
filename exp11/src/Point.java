261f62c321fd20911c4db64a8de1bd77daca0e93
Maerodrim <stas.serg.99@mail.ru>
2019-09-09 23:43:07 +0400

    changes

--- exp11/src/Point.java
+++ exp11/src/Point.java
@@ -1,12 +1,12 @@
-public class Point { 
+public class Point  {
     public final double pointX;
     public final double pointY;
     public final double pointZ;
     public final double length = length(this);
     public Point (double pointX , double pointY , double pointZ){
-        this.pointX=pointX;
-        this.pointY=pointY;
-        this.pointZ=pointZ;
+            this.pointX=pointX;
+            this.pointY=pointY;
+            this.pointZ=pointZ;
     }
     public static double length(Point a){ return (Math.sqrt(a.pointX*a.pointX+a.pointY*a.pointY+a.pointZ*a.pointZ));}
 }
--- exp11/src/Pounts.java
+++ exp11/src/Pounts.java
@@ -1,20 +1,19 @@
 public class Pounts {
     private Pounts() {;}
     public static Point sum(Point a, Point b){
-        Point o= new Point((a.pointX+b.pointX),(a.pointY+b.pointY),(a.pointZ+b.pointZ));
-        return o;}
-    public static Point subtract(Point a, Point b){ Point o= new Point((a.pointX-b.pointX),(a.pointY-b.pointY),(a.pointZ-b.pointZ));
-        return o;}
-    public static Point multiply(Point a, Point b){ Point o= new Point((a.pointX*b.pointX),(a.pointY*b.pointY),(a.pointZ*b.pointZ));
-        return o;}
-    public static Point divide(Point a, Point b){ Point o= new Point((a.pointX/b.pointX),(a.pointY/b.pointY),(a.pointZ/b.pointZ));
-        return o;}
-    public static Point enlarge(Point a, double b){Point o= new Point((a.pointX*b),(a.pointY*b),(a.pointZ*b));
-        return o;}
-    public static Point opposite(Point a){Point o= new Point((-a.pointX),(-a.pointY),(-a.pointZ));
-        return o;}
-    public static Point inverse(Point a){Point o= new Point((1/a.pointX),(1/a.pointY),(1/a.pointZ));
-        return o;}
+        return new Point((a.pointX+b.pointX),(a.pointY+b.pointY),(a.pointZ+b.pointZ));}
+    public static Point subtract(Point a, Point b){
+        return new Point((a.pointX-b.pointX),(a.pointY-b.pointY),(a.pointZ-b.pointZ));}
+    public static Point multiply(Point a, Point b){
+        return new Point((a.pointX*b.pointX),(a.pointY*b.pointY),(a.pointZ*b.pointZ));}
+    public static Point divide(Point a, Point b){
+        return new Point((a.pointX/b.pointX),(a.pointY/b.pointY),(a.pointZ/b.pointZ));}
+    public static Point enlarge(Point a, double b){
+        return  new Point((a.pointX*b),(a.pointY*b),(a.pointZ*b));}
+    public static Point opposite(Point a){
+        return new Point((-a.pointX),(-a.pointY),(-a.pointZ));}
+    public static Point inverse(Point a){
+        return new Point((1/a.pointX),(1/a.pointY),(1/a.pointZ));}
     public static double scalarProduct(Point a, Point b){ return ((a.pointX*b.pointX)+(a.pointY*b.pointY)+(a.pointZ*b.pointZ)); }
     public static Point vectorProduct(Point a, Point b){
         return new Point(a.pointY*b.pointZ - a.pointZ*b.pointY, a.pointZ*b.pointX - a.pointX*b.pointZ, a.pointX*b.pointY-a.pointY*b.pointX);}
